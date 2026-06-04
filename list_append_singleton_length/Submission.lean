66d1f2766155151754d986aa0b3e2fb7ffd3fcae
test1-deepthought <test1.deepthought@gmail.com>
2026-06-04 16:12:22 +1000

    Add Submission.lean for list_append_singleton_length

---
+++ list_append_singleton_length/Submission.lean
@@ -0,0 +1,10 @@
+import Mathlib
+import Submission.Helpers
+
+namespace Submission
+
+theorem list_append_singleton_length :
+    (([1, 2] : List Nat).append [3]).length = 3 := by
+  native_decide
+
+end Submission
