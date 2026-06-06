ac264d4e2b9355594852ac1c3cc0b3ca50261c98
test1-deepthought <test1.deepthought@gmail.com>
2026-06-06 23:22:13 +1000

    Add Lean-Eval workspace exists_chiral_knot

---
+++ exists_chiral_knot/Submission.lean
@@ -0,0 +1,11 @@
+import ChallengeDeps
+import Submission.Helpers
+
+open LeanEval.KnotTheory
+
+namespace Submission
+
+theorem exists_chiral_knot : ∃ K : Knot, K.Chiral := by
+  sorry
+
+end Submission
