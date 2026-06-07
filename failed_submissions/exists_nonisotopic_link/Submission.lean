40cadb8f5c12990fc127bfa76b2249f39393a94f
test1-deepthought <test1.deepthought@gmail.com>
2026-06-07 23:09:54 +1000

    Move exists_nonisotopic_link to failed_submissions - Submission.lean

---
+++ failed_submissions/exists_nonisotopic_link/Submission.lean
@@ -0,0 +1,11 @@
+import ChallengeDeps
+import Submission.Helpers
+
+open LeanEval.KnotTheory
+
+namespace Submission
+
+theorem exists_nonisotopic_link : ∃ L₁ L₂ : TwoLink, ¬ L₁.Isotopic L₂ := by
+  sorry
+
+end Submission
