04a5272aea899c2534e0a988033d852e90f522e8
test1-deepthought <test1.deepthought@gmail.com>
2026-06-07 23:14:35 +1000

    Move abel_ruffini Helpers to failed_submissions

--- failed_submissions/abel_ruffini/Submission/Helpers.lean
+++ failed_submissions/abel_ruffini/Submission/Helpers.lean
@@ -1,6 +1,3 @@
 namespace Submission.Helpers
 
--- Helper lemmas for Abel-Ruffini theorem (partial attempt)
--- This file is a stub; no helpers were completed in this attempt.
-
 end Submission.Helpers
