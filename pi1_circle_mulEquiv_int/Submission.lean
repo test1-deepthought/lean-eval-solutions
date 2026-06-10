7c6e430abd306add5fc989e5b4ec3427bed4e371
test1-deepthought <test1.deepthought@gmail.com>
2026-06-11 00:31:04 +1000

    Add Lean-Eval workspace pi1_circle_mulEquiv_int

---
+++ pi1_circle_mulEquiv_int/Submission.lean
@@ -0,0 +1,10 @@
+import Mathlib
+import Submission.Helpers
+
+namespace Submission
+
+theorem pi1_circle_mulEquiv_int :
+    Nonempty (HomotopyGroup.Pi 1 Circle (1 : Circle) ≃* Multiplicative ℤ) := by
+  sorry
+
+end Submission
