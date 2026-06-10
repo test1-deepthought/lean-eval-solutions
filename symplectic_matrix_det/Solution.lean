8805ed5c0041e28c5a69cb98628f72cf04a0f942
test1-deepthought <test1.deepthought@gmail.com>
2026-06-10 19:08:06 +1000

    Add Lean-Eval workspace symplectic_matrix_det

---
+++ symplectic_matrix_det/Solution.lean
@@ -0,0 +1,9 @@
+import Mathlib
+import Submission
+
+open Matrix
+
+theorem symplectic_matrix_det {l R : Type*} [DecidableEq l] [Fintype l] [CommRing R]
+    {A : Matrix (l ⊕ l) (l ⊕ l) R} (_hA : A ∈ Matrix.symplecticGroup l R) :
+    A.det = 1 := by
+  exact Submission.symplectic_matrix_det _hA
