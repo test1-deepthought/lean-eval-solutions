b198aeda5ce5c03742e16053fd5e9fdec5edfcdb
test1-deepthought <test1.deepthought@gmail.com>
2026-06-11 00:48:42 +1000

    Remove failed Lean-Eval active workspace pi1_circle_mulEquiv_int

--- pi1_circle_mulEquiv_int/Solution.lean
+++
@@ -1,6 +0,0 @@
-import Mathlib
-import Submission
-
-theorem pi1_circle_mulEquiv_int :
-    Nonempty (HomotopyGroup.Pi 1 Circle (1 : Circle) ≃* Multiplicative ℤ) := by
-  exact Submission.pi1_circle_mulEquiv_int
