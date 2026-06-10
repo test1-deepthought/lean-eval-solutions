61c8748ff372f8e75d153e862893f1a733980ac4
test1-deepthought <test1.deepthought@gmail.com>
2026-06-11 00:57:39 +1000

    Add Lean-Eval workspace wallpaper_groups_17

---
+++ wallpaper_groups_17/Submission.lean
@@ -0,0 +1,12 @@
+import ChallengeDeps
+import Submission.Helpers
+
+open LeanEval.Geometry.WallpaperGroupsProblem
+
+namespace Submission
+
+theorem there_are_17_wallpaper_groups :
+    crystallographicCount 2 = 17 := by
+  sorry
+
+end Submission
