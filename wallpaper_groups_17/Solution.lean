4750a1f45486a6b003fa7298b34ee202bd4264f6
test1-deepthought <test1.deepthought@gmail.com>
2026-06-12 22:15:53 +1000

    Remove failed Lean-Eval active workspace wallpaper_groups_17

--- wallpaper_groups_17/Solution.lean
+++
@@ -1,8 +0,0 @@
-import ChallengeDeps
-import Submission
-
-open LeanEval.Geometry.WallpaperGroupsProblem
-
-theorem there_are_17_wallpaper_groups :
-    crystallographicCount 2 = 17 := by
-  exact Submission.there_are_17_wallpaper_groups
