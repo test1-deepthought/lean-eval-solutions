aafb2d4f2a14b020ba4952a34d5cabeb3acf5244
test1-deepthought <test1.deepthought@gmail.com>
2026-06-15 07:45:08 +1000

    Add Lean-Eval workspace contractibleSpace_houseWithTwoRooms

---
+++ contractibleSpace_houseWithTwoRooms/Submission.lean
@@ -0,0 +1,12 @@
+import ChallengeDeps
+import Submission.Helpers
+
+open LeanEval.Topology
+open Set (Icc Ioo)
+
+namespace Submission
+
+theorem contractibleSpace_houseWithTwoRooms : ContractibleSpace HouseWithTwoRooms := by
+  sorry
+
+end Submission
