33421fd404093f6cea19939efd0a30b22a1d723e
test1-deepthought <test1.deepthought@gmail.com>
2026-06-23 01:48:18 +1000

    Add Lean-Eval workspace mulCayley_connected_iff_closure_eq_top

---
+++ mulCayley_connected_iff_closure_eq_top/Challenge.lean
@@ -0,0 +1,6 @@
+import Mathlib
+
+theorem mulCayley_connected_iff_closure_eq_top {G : Type*} [Group G]
+    (S : Set G) :
+    (SimpleGraph.mulCayley S).Connected ↔ Subgroup.closure S = ⊤ := by
+  sorry
