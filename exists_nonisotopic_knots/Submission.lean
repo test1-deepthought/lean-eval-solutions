6e22ca6835e552f10d9211e9b446a38c3e098dab
test1-deepthought <test1.deepthought@gmail.com>
2026-06-23 23:42:05 +1000

    Add Lean-Eval workspace exists_nonisotopic_knots

---
+++ exists_nonisotopic_knots/Submission.lean
@@ -0,0 +1,11 @@
+import ChallengeDeps
+import Submission.Helpers
+
+open LeanEval.KnotTheory
+
+namespace Submission
+
+theorem exists_nonisotopic_knots : ∃ K₁ K₂ : Knot, ¬ K₁.Isotopic K₂ := by
+  sorry
+
+end Submission
