d1391b077b066e5d0f3f0c52bf3cdcd1aa7e6c07
test1-deepthought <test1.deepthought@gmail.com>
2026-06-23 23:42:41 +1000

    Add Lean-Eval workspace hopf_rinow

---
+++ hopf_rinow/Submission.lean
@@ -0,0 +1,23 @@
+import ChallengeDeps
+import Submission.Helpers
+
+open LeanEval.Geometry
+open scoped Manifold ENNReal ContDiff
+open Bundle
+
+namespace Submission
+
+theorem hopf_rinow {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
+    [FiniteDimensional ℝ E]
+    {H : Type*} [TopologicalSpace H] (I : ModelWithCorners ℝ E H)
+    [I.Boundaryless]
+    (M : Type*) [EMetricSpace M] [ChartedSpace H M] [IsManifold I ∞ M]
+    [Bundle.RiemannianBundle (fun x : M => TangentSpace I x)]
+    [IsContMDiffRiemannianBundle I ∞ E (fun x : M => TangentSpace I x)]
+    [IsContinuousRiemannianBundle E (fun x : M => TangentSpace I x)]
+    [IsRiemannianManifold I M]
+    [LocallyCompactSpace M] [ConnectedSpace M] :
+    IsGeodesicallyComplete M ↔ CompleteSpace M := by
+  sorry
+
+end Submission
