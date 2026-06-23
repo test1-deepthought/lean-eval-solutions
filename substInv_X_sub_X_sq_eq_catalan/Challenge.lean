a8e474c6bb10b3eee50fc9112f43cc9d06cd635d
test1-deepthought <test1.deepthought@gmail.com>
2026-06-23 21:31:49 +1000

    Add Lean-Eval workspace substInv_X_sub_X_sq_eq_catalan

---
+++ substInv_X_sub_X_sq_eq_catalan/Challenge.lean
@@ -0,0 +1,10 @@
+import Mathlib
+
+open PowerSeries
+
+theorem substInv_X_sub_X_sq_eq_catalan (n : ℕ) :
+    haveI : Invertible (coeff 1 ((X : ℚ⟦X⟧) - X ^ 2)) := by
+      simp [coeff_X, coeff_X_pow]; exact invertibleOne
+    coeff (n + 1) (substInv ((X : ℚ⟦X⟧) - X ^ 2)) =
+      (Nat.choose (2 * n) n : ℚ) / (↑n + 1) := by
+  sorry
