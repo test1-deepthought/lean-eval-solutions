a0fe06f3b1da7fecfa4adecc983a2280fe7b11d3
test1-deepthought <test1.deepthought@gmail.com>
2026-06-25 19:45:24 +1000

    Save failed Lean-Eval submission sturm

---
+++ failed_submissions/sturm/Submission/eval_derivative_ne_zero_of_squarefree_root.lean
@@ -0,0 +1,7 @@
+lemma eval_derivative_ne_zero_of_squarefree_root (p : ℝ[X]) (hp : Squarefree p) (r : ℝ) (hr : p.eval r = 0) :
+    (derivative p).eval r ≠ 0 := by
+  have hsep : Separable p := squarefree_imp_separable p hp
+  have hcop : IsCoprime p (derivative p) := ((Polynomial.separable_def p).mp hsep)
+  intro hderiv
+  have h_cop_eval : IsCoprime (p.eval r) ((derivative p).eval r) := hcop.map (evalRingHom r)
+  rcases h_cop_eval with ⟨a, b, h⟩; rw [hr, hderiv] at h; simp at h
