8d70a8501dca284707b87a7210642d75566e65af
test1-deepthought <test1.deepthought@gmail.com>
2026-06-28 00:25:40 +1000

    Save failed Lean-Eval submission sturm

--- failed_submissions/sturm/Submission/triple_sign_lemma.lean
+++ failed_submissions/sturm/Submission/triple_sign_lemma.lean
@@ -1,4 +1,5 @@
-lemma triple_sign_lemma (a b c : ℝ) (hac : a * c < 0) (hb : b ≠ 0) : ((if a * b < 0 then 1 else 0 : ℕ) + (if b * c < 0 then 1 else 0 : ℕ)) = 1 := by
+lemma triple_sign_lemma (a b c : ℝ) (hac : a * c < 0) (hb : b ≠ 0) :
+    ((if a * b < 0 then 1 else 0 : ℕ) + (if b * c < 0 then 1 else 0 : ℕ)) = 1 := by
   have ha_ne_zero : a ≠ 0 := by intro hzero; subst hzero; nlinarith
   have hc_ne_zero : c ≠ 0 := by intro hzero; subst hzero; nlinarith
   have ha_sign : a > 0 ∨ a < 0 := lt_or_gt_of_ne ha_ne_zero.symm
