bf79fbbb5e8a7bc1129d854d5abee37f37b6bd88
test1-deepthought <test1.deepthought@gmail.com>
2026-06-28 00:54:10 +1000

    Add Lean-Eval workspace sturm

--- sturm/Submission.lean
+++ sturm/Submission.lean
@@ -3,15 +3,14 @@ import Submission.Helpers
 
 open LeanEval.Algebra
 open Polynomial
-open Set
 open scoped Classical
 
 namespace Submission
 
 theorem sturm (p : ℝ[X]) (hp : Squarefree p) {a b : ℝ} (hab : a < b)
     (ha : p.eval a ≠ 0) (hb : p.eval b ≠ 0) :
-    ((p.roots.toFinset).filter (fun x => a < x ∧ x < b)).card = sigma p a - sigma p b := by
-  -- We'll use the helpers
+    ((p.roots.toFinset).filter (fun x => a < x ∧ x < b)).card =
+      sigma p a - sigma p b := by
   sorry
 
 end Submission
