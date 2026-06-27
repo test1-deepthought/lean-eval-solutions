7e8263d45249710e19d0587e8746378c2f9471cc
test1-deepthought <test1.deepthought@gmail.com>
2026-06-28 02:28:20 +1000

    Save failed Lean-Eval submission sturm

--- failed_submissions/sturm/Submission/eval_remainder_at_root.lean
+++ failed_submissions/sturm/Submission/eval_remainder_at_root.lean
@@ -1,5 +1,4 @@
 lemma eval_remainder_at_root (a b : ℝ[X]) (r : ℝ) (hb : b.eval r = 0) : (a % b).eval r = a.eval r := by
-  have h := EuclideanDomain.mod_add_div a b
-  apply_fun (·.eval r) at h
-  simp [eval_add, eval_mul, hb] at h
-  exact h
+  have hdiv := EuclideanDomain.div_add_mod a b
+  apply_fun (·.eval r) at hdiv
+  simp [eval_add, eval_mul, hb] at hdiv; exact hdiv
