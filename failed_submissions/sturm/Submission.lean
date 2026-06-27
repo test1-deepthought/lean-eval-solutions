c32c22fb3d9758fa6dd6478c6396869cd73f3f87
test1-deepthought <test1.deepthought@gmail.com>
2026-06-27 13:32:11 +1000

    Save failed Lean-Eval submission sturm

--- failed_submissions/sturm/Submission.lean
+++ failed_submissions/sturm/Submission.lean
@@ -3,19 +3,14 @@ import Submission.Helpers
 
 open LeanEval.Algebra
 open Polynomial
-open Set
 open scoped Classical
 
 namespace Submission
 
-noncomputable def sigmaAux (a b : ℝ[X]) (n : ℕ) (x : ℝ) : ℕ :=
-  signChanges ((sturmAux a b n).map (·.eval x))
-
-lemma sigma_eq_sigmaAux (p : ℝ[X]) (x : ℝ) : sigma p x = sigmaAux p (derivative p) (p.natDegree + 2) x := by
-  unfold sigma sigmaAux sturmChain; rfl
-
-lemma pderiv_ne_zero_at_root (p : ℝ[X]) (hp : Squarefree p) (r : ℝ) (hr : p.eval r = 0) :
-    (derivative p).eval r ≠ 0 :=
-  Helpers.eval_derivative_ne_zero_of_squarefree_root p hp r hr
+theorem sturm (p : ℝ[X]) (hp : Squarefree p) {a b : ℝ} (hab : a < b)
+    (ha : p.eval a ≠ 0) (hb : p.eval b ≠ 0) :
+    ((p.roots.toFinset).filter (fun x => a < x ∧ x < b)).card =
+      sigma p a - sigma p b := by
+  sorry
 
 end Submission
