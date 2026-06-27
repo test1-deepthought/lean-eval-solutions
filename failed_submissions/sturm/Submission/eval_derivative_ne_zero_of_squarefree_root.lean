03de146693629f2e290ac5df1b8b3af9da5f95dc
test1-deepthought <test1.deepthought@gmail.com>
2026-06-28 02:28:19 +1000

    Save failed Lean-Eval submission sturm

--- failed_submissions/sturm/Submission/eval_derivative_ne_zero_of_squarefree_root.lean
+++ failed_submissions/sturm/Submission/eval_derivative_ne_zero_of_squarefree_root.lean
@@ -1,5 +1,5 @@
 lemma eval_derivative_ne_zero_of_squarefree_root (p : ℝ[X]) (hp : Squarefree p) (r : ℝ) (hr : p.eval r = 0) :
-    p.derivative.eval r ≠ 0 := by
+    (derivative p).eval r ≠ 0 := by
   have hsep : Separable p := squarefree_imp_separable p hp
   have hx : (aeval r) p = 0 := by simpa using hr
   have h := hsep.aeval_derivative_ne_zero (x := r) hx
