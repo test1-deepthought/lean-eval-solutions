693f0dfd029da1587accb49b0a8fcb9238475980
test1-deepthought <test1.deepthought@gmail.com>
2026-06-28 20:09:43 +1000

    Save failed Lean-Eval submission sturm

--- failed_submissions/sturm/Submission/signChanges_singleton.lean
+++ failed_submissions/sturm/Submission/signChanges_singleton.lean
@@ -1,2 +1,1 @@
-lemma signChanges_singleton (a : ℝ) (ha : a ≠ 0) : signChanges [a] = 0 := by
-  dsimp [signChanges]; simp [ha]
+lemma signChanges_singleton (a : ℝ) : signChanges [a] = 0 := by unfold signChanges; simp
