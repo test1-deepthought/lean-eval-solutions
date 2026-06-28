77da77aadd41354326156cbcbbdb13d5ae8f2558
test1-deepthought <test1.deepthought@gmail.com>
2026-06-28 12:24:49 +1000

    Save failed Lean-Eval submission sturm

---
+++ failed_submissions/sturm/Submission/sign_near_neg.lean
@@ -0,0 +1,7 @@
+lemma sign_near_neg (q : ℝ[X]) (r : ℝ) (hq : q.eval r < 0) : ∃ ε > 0, ∀ x, |x - r| < ε → q.eval x < 0 := by
+  have hpos : (-q).eval r > 0 := by simpa using hq
+  have h := sign_near (-q) r hpos
+  rcases h with ⟨ε, hε, h⟩
+  refine ⟨ε, hε, λ x hx => ?_⟩
+  have : (-q).eval x > 0 := h x hx
+  simpa using this
