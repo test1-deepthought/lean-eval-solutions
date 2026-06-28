10ffd47463ecf46664183c142d1c93285756873b
test1-deepthought <test1.deepthought@gmail.com>
2026-06-28 21:58:30 +1000

    Save failed Lean-Eval submission sturm

--- failed_submissions/sturm/Submission/squarefree_no_common_root.lean
+++ failed_submissions/sturm/Submission/squarefree_no_common_root.lean
@@ -1,7 +1,6 @@
 lemma squarefree_no_common_root (p : ℝ[X]) (hp : Squarefree p) (r : ℝ) (hr : p.eval r = 0) :
     (derivative p).eval r ≠ 0 := by
-  have hsep : p.Separable := by
-    have h := PerfectField.separable_iff_squarefree (K := ℝ) (g := p); exact h.mpr hp
+  have hsep : p.Separable := squarefree_imp_separable p hp
   have h_coprime : IsCoprime p (derivative p) := ((Polynomial.separable_def (f := p)).mp hsep)
   rcases h_coprime with ⟨a, b, h⟩
   have h_eval := congrArg (fun q => q.eval r) h
