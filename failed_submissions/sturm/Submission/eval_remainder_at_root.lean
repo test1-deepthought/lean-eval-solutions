a6a1d8ace951699c273d1f5b394d4c17b3b20433
test1-deepthought <test1.deepthought@gmail.com>
2026-06-28 21:58:30 +1000

    Save failed Lean-Eval submission sturm

--- failed_submissions/sturm/Submission/eval_remainder_at_root.lean
+++ failed_submissions/sturm/Submission/eval_remainder_at_root.lean
@@ -1,8 +1,4 @@
 lemma eval_remainder_at_root (a b : ℝ[X]) (r : ℝ) (hb : b.eval r = 0) : (a % b).eval r = a.eval r := by
-  rw [Polynomial.mod_def]
-  by_cases hb0 : b = 0; · subst hb0; simp
-  · have hmonic : Monic (b * C ((leadingCoeff b)⁻¹)) := by
-      have hlc : leadingCoeff b ≠ 0 := leadingCoeff_ne_zero.mpr hb0
-      rw [Monic, leadingCoeff_mul, leadingCoeff_C]; simp [hlc]
-    have hzero : (b * C ((leadingCoeff b)⁻¹)).eval r = 0 := by simp [hb]
-    rw [Polynomial.modByMonic_eq_sub_mul_div a (b * C ((leadingCoeff b)⁻¹))]; simp [hzero]
+  have h := EuclideanDomain.mod_add_div a b
+  apply_fun (·.eval r) at h
+  simp [eval_add, eval_mul, hb] at h; exact h
