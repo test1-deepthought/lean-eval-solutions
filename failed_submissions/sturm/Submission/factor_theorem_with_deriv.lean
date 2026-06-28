33c62eb50b80b545112bf8787ad88bcb0ffa6385
test1-deepthought <test1.deepthought@gmail.com>
2026-06-28 20:09:41 +1000

    Save failed Lean-Eval submission sturm

--- failed_submissions/sturm/Submission/factor_theorem_with_deriv.lean
+++ failed_submissions/sturm/Submission/factor_theorem_with_deriv.lean
@@ -1,10 +1,12 @@
-lemma factor_theorem_with_deriv (p : ℝ[X]) (r : ℝ) (hp0 : p.eval r = 0) : ∃ q : ℝ[X], p = (X - C r) * q ∧ q.eval r = (derivative p).eval r := by
-  have hdiv : (X - C r) ∣ p := Polynomial.dvd_iff_isRoot.mpr hp0
-  rcases hdiv with ⟨q, h⟩
-  have hcalc : q.eval r = (derivative p).eval r := by
-    have hderiv : derivative p = q + (X - C r) * derivative q := by
-      rw [h]; rw [derivative_mul]; simp
+lemma factor_theorem_with_deriv (p : ℝ[X]) (r : ℝ) (hr : p.eval r = 0) :
+    ∃ q : ℝ[X], p = (X - C r) * q ∧ q.eval r = p.derivative.eval r := by
+  have hroot : IsRoot p r := by rw [IsRoot, hr]
+  have hdiv : (X - C r) ∣ p := (Polynomial.dvd_iff_isRoot).mpr hroot
+  rcases hdiv with ⟨q, hp_eq⟩
+  have hqeval : q.eval r = p.derivative.eval r := by
     calc
       q.eval r = (q + (X - C r) * derivative q).eval r := by simp
-      _ = (derivative p).eval r := by rw [hderiv]
-  exact ⟨q, h, hcalc⟩
+      _ = (derivative ((X - C r) * q)).eval r := by
+        rw [derivative_mul, derivative_sub, derivative_X, derivative_C]; simp
+      _ = (derivative p).eval r := by rw [hp_eq]
+  refine ⟨q, hp_eq, hqeval⟩
