8e00a334ef37155cf507576e8d6fcded4ca85edf
test1-deepthought <test1.deepthought@gmail.com>
2026-06-28 12:24:51 +1000

    Save failed Lean-Eval submission sturm

--- failed_submissions/sturm/Submission/eval_derivative_ne_zero_of_squarefree_root.lean
+++ failed_submissions/sturm/Submission/eval_derivative_ne_zero_of_squarefree_root.lean
@@ -1,6 +1,9 @@
-lemma eval_derivative_ne_zero_of_squarefree_root (p : ℝ[X]) (hp : Squarefree p) (r : ℝ) (hr : p.eval r = 0) :
-    (derivative p).eval r ≠ 0 := by
-  have hsep : Separable p := squarefree_imp_separable p hp
-  have hx : (aeval r) p = 0 := by simpa using hr
-  have h := hsep.aeval_derivative_ne_zero (x := r) hx
-  simpa using h
+lemma eval_derivative_ne_zero_of_squarefree_root (p : ℝ[X]) (hp : Squarefree p) (r : ℝ) (hr : p.eval r = 0) : 
+    p.derivative.eval r ≠ 0 := by
+  have hsep : p.Separable := ((PerfectField.separable_iff_squarefree (K := ℝ) (g := p)).mpr hp)
+  rcases ((Polynomial.separable_def p).mp hsep) with ⟨a, b, h⟩
+  have h_eval : (a * p + b * derivative p).eval r = 1 := by rw [h, Polynomial.eval_one]
+  have h_eval' : (a * p + b * derivative p).eval r = a.eval r * p.eval r + b.eval r * (derivative p).eval r := by
+    simp [Polynomial.eval_add, Polynomial.eval_mul]
+  rw [h_eval'] at h_eval; rw [hr] at h_eval; simp at h_eval
+  intro hzero; rw [hzero] at h_eval; simp at h_eval
