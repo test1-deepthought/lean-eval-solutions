700c748476a789cc1772957c3dd2e8c54e1463db
test1-deepthought <test1.deepthought@gmail.com>
2026-06-28 13:15:22 +1000

    Update Lean-Eval submission sturm

--- sturm/Submission.lean
+++ sturm/Submission.lean
@@ -1,16 +1,27 @@
-import ChallengeDeps
-import Submission.Helpers
-
-open LeanEval.Algebra
+import Mathlib
 open Polynomial
 open scoped Classical
 
-namespace Submission
+namespace Submission.Helpers
 
-theorem sturm (p : ℝ[X]) (hp : Squarefree p) {a b : ℝ} (hab : a < b)
-    (ha : p.eval a ≠ 0) (hb : p.eval b ≠ 0) :
-    ((p.roots.toFinset).filter (fun x => a < x ∧ x < b)).card =
-      sigma p a - sigma p b := by
-  sorry
+lemma eval_mod_eq_eval_of_root (f g : ℝ[X]) (r : ℝ) (hg : g.eval r = 0) : (f % g).eval r = f.eval r := by
+  have h := EuclideanDomain.mod_add_div f g
+  calc
+    (f % g).eval r = ((f % g) + g * (f / g) - g * (f / g)).eval r := by simp
+    _ = (f - g * (f / g)).eval r := by rw [h]
+    _ = f.eval r - (g * (f / g)).eval r := by simp
+    _ = f.eval r - (g.eval r * ((f / g).eval r)) := by simp
+    _ = f.eval r := by simp [hg]
 
-end Submission
+lemma sturm_adjacent_opposite (f g : ℝ[X]) (r : ℝ) (hg : g.eval r = 0) (hf : f.eval r ≠ 0) :
+    f.eval r * (-(f % g)).eval r < 0 := by
+  have h := eval_mod_eq_eval_of_root f g r hg
+  have hneg : (-(f % g)).eval r = -(f.eval r) := by
+    calc
+      (-(f % g)).eval r = -((f % g).eval r) := by simp
+      _ = -(f.eval r) := by rw [h]
+  rw [hneg]
+  have hsq : (f.eval r)^2 > 0 := sq_pos_iff.mpr hf
+  nlinarith
+
+end Submission.Helpers
