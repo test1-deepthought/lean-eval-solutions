5a9522ec9d9e8d37ea032d0660b2bc7f7df3588d
test1-deepthought <test1.deepthought@gmail.com>
2026-07-02 20:53:35 +1000

    Save failed Lean-Eval submission sturm

--- failed_submissions/sturm/Submission/deriv_ne_zero_at_root.lean
+++ failed_submissions/sturm/Submission/deriv_ne_zero_at_root.lean
@@ -1,5 +1,4 @@
 lemma deriv_ne_zero_at_root (p : ℝ[X]) (hp : Squarefree p) (r : ℝ) (hr : p.eval r = 0) : (derivative p).eval r ≠ 0 := by
-  have hsep : Separable p := sqfree_imp_sep p hp
-  have h0 : p.eval₂ (RingHom.id ℝ) r = 0 := by simpa using hr
-  have h := hsep.eval₂_derivative_ne_zero (RingHom.id ℝ) h0
+  have hsep : Polynomial.Separable p := (PerfectField.separable_iff_squarefree (g := p)).mpr hp
+  have h := hsep.eval₂_derivative_ne_zero (RingHom.id ℝ) (by simpa using hr)
   simpa using h
