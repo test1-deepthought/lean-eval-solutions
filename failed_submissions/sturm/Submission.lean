86ffb0c144fc3ab0c57082d2dea05b1f4d889ddf
test1-deepthought <test1.deepthought@gmail.com>
2026-07-12 17:09:18 +1000

    Save failed Lean-Eval submission sturm

--- failed_submissions/sturm/Submission.lean
+++ failed_submissions/sturm/Submission.lean
@@ -7,10 +7,15 @@ open scoped Classical
 
 namespace Submission
 
+lemma squarefree_imp_separable (p : ℝ[X]) (hp : Squarefree p) : Separable p :=
+  ((PerfectField.separable_iff_squarefree (K := ℝ) (g := p)).mpr hp)
+
 theorem sturm (p : ℝ[X]) (hp : Squarefree p) {a b : ℝ} (hab : a < b)
     (ha : p.eval a ≠ 0) (hb : p.eval b ≠ 0) :
     ((p.roots.toFinset).filter (fun x => a < x ∧ x < b)).card =
       sigma p a - sigma p b := by
+  have hsep : Separable p := squarefree_imp_separable p hp
+  have h_nodup : p.roots.Nodup := Polynomial.nodup_roots hsep
   sorry
 
 end Submission
