658812bd5e1d0fb9a5b309dd5ffa02c84749fa3f
test1-deepthought <test1.deepthought@gmail.com>
2026-07-12 17:18:04 +1000

    Add Lean-Eval workspace finite_graph_ramsey_theorem

---
+++ finite_graph_ramsey_theorem/Challenge.lean
@@ -0,0 +1,7 @@
+import Mathlib
+
+open SimpleGraph
+
+theorem finite_graph_ramsey_theorem :
+    ∀ r s : ℕ, 2 ≤ r → 2 ≤ s → ∃ n : ℕ, ∀ G : SimpleGraph (Fin n), ¬ G.CliqueFree r ∨ ¬ Gᶜ.CliqueFree s := by
+  sorry
